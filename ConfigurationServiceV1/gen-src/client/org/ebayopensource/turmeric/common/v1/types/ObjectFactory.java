336b54ccbabccee4df9a2194c8acb37638b7fd90
Tiago Espinha <tiago@espinhas.net>
2011-08-27 18:52:01 +0200

    Reverting WSDL

--- ConfigurationServiceV1/gen-src/client/org/ebayopensource/turmeric/common/v1/types/ObjectFactory.java
+++ ConfigurationServiceV1/gen-src/client/org/ebayopensource/turmeric/common/v1/types/ObjectFactory.java
@@ -30,27 +30,11 @@ public class ObjectFactory {
     }
 
     /**
-     * Create an instance of {@link ExtensionType }
-     * 
-     */
-    public ExtensionType createExtensionType() {
-        return new ExtensionType();
-    }
-
-    /**
      * Create an instance of {@link CommonErrorData }
      * 
      */
     public CommonErrorData createCommonErrorData() {
         return new CommonErrorData();
-    }
-
-    /**
-     * Create an instance of {@link ErrorParameter }
-     * 
-     */
-    public ErrorParameter createErrorParameter() {
-        return new ErrorParameter();
     }
 
     /**
@@ -67,6 +51,22 @@ public class ObjectFactory {
      */
     public ErrorMessage createErrorMessage() {
         return new ErrorMessage();
+    }
+
+    /**
+     * Create an instance of {@link ErrorParameter }
+     * 
+     */
+    public ErrorParameter createErrorParameter() {
+        return new ErrorParameter();
+    }
+
+    /**
+     * Create an instance of {@link ExtensionType }
+     * 
+     */
+    public ExtensionType createExtensionType() {
+        return new ExtensionType();
     }
 
 }
