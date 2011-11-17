95ec025dc796b94b69ed1027708d777f34ad47c7
alpsmoon <stillwatersct@msn.com>
2011-11-17 11:05:28 +0100

    fill in several methods

--- BusinessServiceV1Impl/src/nl/tudelft/ewi/st/atlantis/tudelft/v1/services/businessservice/test/TraderServiceManagerTest.java
+++ BusinessServiceV1Impl/src/nl/tudelft/ewi/st/atlantis/tudelft/v1/services/businessservice/test/TraderServiceManagerTest.java
@@ -8,10 +8,13 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Properties;
 
 import nl.tudelft.ewi.st.atlantis.tudelft.v1.services.businessservice.impl.TraderServiceManager;
 import nl.tudelft.stocktrader.Account;
+import nl.tudelft.stocktrader.AccountProfile;
+import nl.tudelft.stocktrader.Order;
 import nl.tudelft.stocktrader.dal.DAOException;
 import nl.tudelft.stocktrader.dal.DAOFactory;
 
@@ -122,98 +125,187 @@ public class TraderServiceManagerTest {
 	}
 
 	@Test
-	public void testLogout() {
+	public void testLogout() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		t.logout("uid:0");
+		
+		Account a = t.getAccountData("uid:0");
+		assertTrue("Logout doesn't match expected", a.getLogoutCount()!=14);
 		
 	}
 
 	@Test
-	public void testGetAccountData() {
-		fail("Not yet implemented");
+	public void testGetAccountData() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		Account a = t.getAccountData("uid:0");
+		
+		assertTrue("getAccountData failed", a != null);
+
+		Calendar creationDate = Calendar.getInstance();
+		creationDate.set(2011, 9, 8, 0, 0, 0);
+		assertTrue("CreationDate of Account does not match expected", a.getCreationDate().equals(creationDate));
+		assertTrue("OpenBalance of Account does not match expected", a.getOpenBalance().equals(BigDecimal.valueOf(100.00)));
+		assertEquals("LogoutCount of Account does not match expected", (double)13, (double)a.getLogoutCount());
+		assertTrue("Balance of Account does not match expected", a.getBalance().equals(BigDecimal.valueOf(200.00)));
+		assertEquals("AccountID of Account does not match expected", (double)1, (double)a.getAccountID());
+		Calendar lastLogin = Calendar.getInstance();
+		lastLogin.set(2011, 11, 11, 15, 49, 38);
+		assertTrue("LastLogin of Account does not match expected", a.getLastLogin().equals(lastLogin));
+		assertEquals("LoginCount of Account does not match expected", (double)36, (double)a.getLoginCount());
+		assertTrue("UserID of Account does not match expected", a.getUserID().equals("uid:0"));
+		assertTrue("ProfileID of Account does not match expected", a.getProfileID().equals("uid:0"));
+		assertTrue("CurrencyType of Account does not match expected", a.getCurrencyType().equals("USD"));
+		
 	}
 
 	@Test
-	public void testGetAccountProfileData() {
-		fail("Not yet implemented");
+	public void testGetAccountProfileData() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		AccountProfile ap = t.getAccountProfileData("uid:0");
+		
+		assertTrue("getAccountProfileData failed", ap != null);
+
+		assertTrue("Address of AccountProfile does not match expected", ap.getAddress().equals("blah"));
+		assertTrue("Password of AccountProfile does not match expected", ap.getPassword().equals("xxx"));
+		assertTrue("UserID of AccountProfile does not match expected", ap.getUserID().equals("uid:0"));
+		assertTrue("Email of AccountProfile does not match expected", ap.getEmail().equals("blah"));
+		assertTrue("Creditcard of AccountProfile does not match expected", ap.getCreditCard().equals("21321321321"));
+		assertTrue("Fullname of AccountProfile does not match expected", ap.getFullName().equals("Tiago"));
+		
 	}
 
 	@Test
-	public void testGetOrders() {
-		fail("Not yet implemented");
+	public void testGetOrders() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		List<Order> orderList = t.getOrders("uid:0");
+		assertTrue("getOrders failed", orderList != null);
+		assertTrue("The number of orders does not match expected", orderList.size()==2);
+		for(int i=0; i<2; i++){
+			Order o = orderList.get(i);
+			if(o.getOrderID() == 1){
+				assertTrue("OrderFee of Order 1 does not match expected", o.getOrderFee().equals(BigDecimal.valueOf(15.95)));
+				assertTrue("OrderType of Order 1 does not match expected", o.getOrderType().equals("buy"));
+				assertTrue("OrderStatus of Order 1 does not match expected", o.getOrderStatus().equals("open"));
+				assertTrue("Price of Order 1 does not match expected", o.getPrice().equals(BigDecimal.valueOf(1.00)));
+				assertTrue("Quantity of Order 1 does not match expected", o.getQuantity().equals(10.0));
+				Calendar openDate = Calendar.getInstance();
+				openDate.set(2011, 9, 15, 15, 55, 18);
+				assertTrue("OpenDate of Order 1 does not match expected", o.getOpenDate().equals(openDate));
+				assertTrue("OrderID of Order 1 does not match expected", o.getOrderID()==1);
+				assertTrue("AccountID of Order 1 does not match expected", o.getAccountId()==1);
+				assertTrue("Quote_Symbol of Order 1 does not match expected", o.getSymbol().equals("s:AAPL"));
+				assertTrue("HoldingID of Order 1 does not match expected", o.getHoldingId()== -1);				
+				
+			}else if(o.getOrderID() == 2){
+				assertTrue("OrderFee of Order 2 does not match expected", o.getOrderFee().equals(BigDecimal.valueOf(15.95)));
+				assertTrue("OrderType of Order 2 does not match expected", o.getOrderType().equals("buy"));
+				assertTrue("OrderStatus of Order 2 does not match expected", o.getOrderStatus().equals("closed"));
+				assertTrue("Price of Order 2 does not match expected", o.getPrice().equals(BigDecimal.valueOf(1.00)));
+				assertTrue("Quantity of Order 2 does not match expected", o.getQuantity().equals(123.0));
+				Calendar openDate = Calendar.getInstance();
+				openDate.set(2011, 9, 15, 15, 55, 54);
+				assertTrue("OpenDate of Order 2 does not match expected", o.getOpenDate().equals(openDate));
+				assertTrue("OrderID of Order 2 does not match expected", o.getOrderID()==2);
+				assertTrue("AccountID of Order 2 does not match expected", o.getAccountId()==1);
+				assertTrue("Quote_Symbol of Order 2 does not match expected", o.getSymbol().equals("s:AAPL"));
+				assertTrue("HoldingID of Order 2 does not match expected", o.getHoldingId()== -1);	
+				
+			}else{
+				assertTrue("Order information does not match expected", false);
+			}
+		}	
 	}
 
 	@Test
-	public void testGetTopOrders() {
-		fail("Not yet implemented");
+	public void testGetTopOrders() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		List<Order> topOrders = t.getTopOrders("uid:0");
+		assertTrue("getTopOrders failed", topOrders != null);
+		
+		
 	}
 
 	@Test
 	public void testGetClosedOrders() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
+		assertTrue("getClosedOrders failed", a != null);
 	}
 
 	@Test
-	public void testRegister() {
-		fail("Not yet implemented");
+	public void testRegister() throws DAOException {
+		TraderServiceManager t = new TraderServiceManager();
+		String result = t.register("uid:33", "xxx", "blah", "blah", "blah", "666666666", BigDecimal.valueOf(10000.00), "USD");
+		
+		assertTrue("register function failed completely", result != null);
+		assertTrue("register function failed", result.equals("success"));
+		
+		result = t.register("uid:33", "xxx", "blah", "blah", "blah", "666666666", BigDecimal.valueOf(10000.00), "USD");
+		assertTrue("register function failed to check user exist", result.equals("userexist"));
+		
+		result = t.register(null, "xxx", "blah", "blah", "blah", "666666666", BigDecimal.valueOf(10000.00), "USD");
+		assertTrue("register function failed to send back failed message", result.equals("failed"));
+		
 	}
 
 	@Test
 	public void testUpdateAccountProfile() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
+		assertTrue("getAccountData failed", a != null);
 	}
 
 	@Test
 	public void testGetMarketSummary() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testGetQuote() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testGetAllQuotes() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testBuy() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testSell() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testSellEnhanced() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testPlaceOrder() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testGetHolding() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testGetHoldings() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testGetWallet() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 	@Test
 	public void testUpdateWallet() {
-		fail("Not yet implemented");
+		TraderServiceManager t = new TraderServiceManager();
 	}
 
 }
